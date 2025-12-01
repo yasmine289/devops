2aa8a17911ff89f448feb46cf8830d940a782bc5
hammamiyasmine <hammamiyasmine2890@gmail.com>
2025-12-01 10:07:20 +0000

    modifier msg

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello yasminaa, from new_feature");
+        System.out.println("Hello DSI31");
     }
 }
